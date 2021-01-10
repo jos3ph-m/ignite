3e9b4283beb25cf33db75a19c89a4037dabef261
Joseph M <jmedina1985@hotmail.com>
2021-01-09 22:43:41 -0800

    Setting up redux - 2

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+  popular = [],
+  newGames = [],
+  upcoming = []
+}
+
+const gameReducer = (state=initState, action) => {
+  switch(action.type) {
+    case "FETCH_GAMES":
+      return {...state}
+    default: 
+      return {...state}
+  }
+}
