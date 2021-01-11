27af9e7afcec83c478e7eabba5af2fb343555005
Joseph M <jmedina1985@hotmail.com>
2021-01-11 11:30:47 -0800

    games action updated with newGames and upcoming

--- src/api.js
+++ src/api.js
@@ -35,3 +35,4 @@ const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&pag
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${new_games}`;
