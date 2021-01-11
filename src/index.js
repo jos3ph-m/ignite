738d20af6d52ae771da3b6a1e86ddd6ee234ff94
Joseph M <jmedina1985@hotmail.com>
2021-01-10 22:24:22 -0800

    Copy and pasted redux devtools extension compose

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-const composeEnhancer =
-  window.__REDUX_DEVTOOLS_EXTENSION__ /
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
