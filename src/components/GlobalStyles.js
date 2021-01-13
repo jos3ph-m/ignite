c8c788a887303069b4f0a07a73600da476b864b5
Joseph M <jmedina1985@hotmail.com>
2021-01-12 23:14:29 -0800

    More global styling - 2

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -23,6 +23,12 @@ const GlobalStyles = createGlobalStyle`
     font-size: 3rem;
     font-family: "Abril Fatface", cursive;
     font-weight: lighter;
+    color: #333;
+  }
+  h3 {
+    font-size: 1.3rem;
+    color: #333;
+    padding: 1.5rem;
   }
 `;
 
