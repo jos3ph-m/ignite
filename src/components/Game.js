c95abb2adad21333ff01b2277d71f09a9f620c51
Joseph M <jmedina1985@hotmail.com>
2021-01-12 23:01:46 -0800

    Fixing cards

--- src/components/Game.js
+++ src/components/Game.js
@@ -18,8 +18,12 @@ const Game = ({ name, released, image, rating }) => {
 const StyledGame = styled(motion.div)`
   min-height: 30vh;
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 3%;
   img {
     width: 100%;
+    height: 40vh;
+    object-fit: cover;
   }
 `;
 
