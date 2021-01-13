cb6585057181d021e937267fb7549aaaed398c4a
Joseph M <jmedina1985@hotmail.com>
2021-01-12 21:21:55 -0800

    Some grid action

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -48,6 +48,8 @@ const Games = styled(motion.div)`
   min-height: 80vh;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
 `;
 
 export default Home;
