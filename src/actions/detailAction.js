5d0018f9dab51615af17d2856a35fce22ce4a565
Joseph M <jmedina1985@hotmail.com>
2021-01-13 23:27:53 -0800

    dispatch get_detail

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -3,4 +3,11 @@ import { gameDetailsURL } from '../api';
 
 const loadDetail = (id) => async (dispatch) => {
   const detailData = await axios.get(gameDetailsURL(id));
+
+  dispatch({
+    type: 'GET_DETAIL',
+    payload: {
+      game: detailData.data,
+    },
+  });
 };
