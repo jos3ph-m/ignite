1f0163a1365ef34499cc09a13b5a229641919d9b
Joseph M <jmedina1985@hotmail.com>
2021-01-13 23:14:30 -0800

    gameDetailsURL plus action

---
+++ src/actions/detailAction.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { gameDetailsURL } from '../api';
+
+const loadDetail = (id) => async (dispatch) => {
+  const detailData = await axios.get(gameDetailsURL(id));
+};
--- src/api.js
+++ src/api.js
@@ -36,3 +36,6 @@ const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&pag
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
+
+// Game details
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
