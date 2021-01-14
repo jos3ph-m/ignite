b0bfa3aa73807a9d98a907504b6f21f4ef6832bb
Joseph M <jmedina1985@hotmail.com>
2021-01-13 23:30:47 -0800

    detail reducer

---
+++ src/reducers/detailReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+  game: {},
+};
+
+const detailReducer = (state=initialState, action) => {
+  switch(action.type) {
+    case "GET_DETAIL": 
+      return{
+        ...state,
+        game: action.payload.game
+      }
+    default {
+      return {
+        ...state
+      }
+    }  
+  }
+}
+
+export default detailReducer;
