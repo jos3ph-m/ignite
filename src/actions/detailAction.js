344b00085bca00bae61cb5edd944e7dcfb32aec4
Joseph M <jmedina1985@hotmail.com>
2021-01-14 17:09:55 -0800

    screenshot added to detailAction

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import { gameDetailsURL } from '../api';
+import { gameDetailsURL, gameScreenshotURL } from '../api';
 
 export const loadDetail = (id) => async (dispatch) => {
   const detailData = await axios.get(gameDetailsURL(id));
+  const screenshotData = await axios.get(gameScreenshotURL(id));
 
   dispatch({
     type: 'GET_DETAIL',
