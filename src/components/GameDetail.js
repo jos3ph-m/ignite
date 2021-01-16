91c59939418ccefc64c0ea8b3c44d45283c14ac4
Joseph M <jmedina1985@hotmail.com>
2021-01-15 23:47:15 -0800

    GameDetail.js - 4

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -10,7 +10,20 @@ const GameDetail = () => {
   // Data
   const { screen, game } = useSelector((state) => state.detail);
   return (
-
+    <div className="card-shadow">
+      <div className="detail">
+        <div className="stats">
+          <div className="rating">
+            <h3>{game.name}</h3>
+            <p>Rating: {game.rating}</p>
+          </div>
+          <div className="info">
+            <h3>Platforms</h3>
+            <div className="platforms"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
