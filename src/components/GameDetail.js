0ce1c56b8ed5376b8c6f43ecd6abf9452947a07c
Joseph M <jmedina1985@hotmail.com>
2021-01-15 23:44:13 -0800

    GameDetail.js - 2

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -2,3 +2,10 @@ import React from 'react';
 // Styling and Animation
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+// Redux
+import { useSelector } from 'react-redux';
+
+const GameDetail = () => {
+  // Data
+  const detail = useSelector((state) => state.detail);
+};
