52dfa63c5ed87164228fb0892a98946d3454da57
Joseph M <jmedina1985@hotmail.com>
2021-01-17 23:55:21 -0800

    Adding react router - 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // Components and pages
 import Home from './pages/Home';
 import GlobalStyles from './components/GlobalStyles';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
