09bdbe2b6529644c8a48773b8388ce0ab4f00686
Joseph M <jmedina1985@hotmail.com>
2021-01-17 23:58:13 -0800

    Adding react router - 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <Home />
+      <Route path={['/game/:id', '/']}>
+        <Home />
+      </Route>
     </div>
   );
 }
