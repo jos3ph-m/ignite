34214a8f951b68e634c19182bc59f7e943292fe3
Joseph M <jmedina1985@hotmail.com>
2021-01-19 12:05:50 -0800

    Image resize utility - 3

--- src/util.js
+++ src/util.js
@@ -7,4 +7,5 @@ export const smallImage = (imagePath, size) => {
         `media/resize/${size}/-/screenshots`
       )
     : imagePath.replace('/media/games/', `media/resize/${size}/-/games/`);
+  return image;
 };
