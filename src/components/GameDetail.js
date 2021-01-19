1cfdd882afb4ba8cc721f25662b71f2ba0b62775
Joseph M <jmedina1985@hotmail.com>
2021-01-19 00:07:21 -0800

    exit detail handler - 2

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -4,11 +4,17 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 // Redux
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const GameDetail = () => {
+  const history = useHistory();
   // Exit Detail
   const exitDetailHandler = (e) => {
     const element = e.target;
+    if (element.classList.contains('shadow')) {
+      document.body.style.overflow = 'auto';
+      history.push('/');
+    }
   };
 
   // Data
@@ -16,7 +22,7 @@ const GameDetail = () => {
   return (
     <>
       {!isLoading && (
-        <CardShadow>
+        <CardShadow className="shadow" onClick={exitDetailHandler}>
           <Detail>
             <Stats>
               <div className="rating">
