f46a0d71f56515e487ff3d6de518d9fed0c1a2a9
Joseph M <jmedina1985@hotmail.com>
2021-01-21 22:46:50 -0800

    Adding icons - 1

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -7,6 +7,13 @@ import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 // util
 import { smallImage } from '../util';
+// Images
+import playstation from '../img/playstation.svg';
+import steam from '../img/steam.svg';
+import xbox from '../img/xbox.svg';
+import nintendo from '../img/nintendo.svg';
+import apple from '../img/apple.svg';
+import gamepad from '../img/gamepad.svg';
 
 const GameDetail = ({ pathId }) => {
   const history = useHistory();
