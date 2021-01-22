3fe41ac0ff3e75875feae06805dd552d33f429ac
Joseph M <jmedina1985@hotmail.com>
2021-01-21 23:28:00 -0800

    Adding icons - 5

--- src/util.js
+++ src/util.js
@@ -1,11 +1,14 @@
 // Media resize
 
 export const smallImage = (imagePath, size) => {
-  const image = imagePath.match(/media\/screenshots/)
-    ? imagePath.replace(
-        'media/screenshots',
-        `media/resize/${size}/-/screenshots`
-      )
-    : imagePath.replace('media/games/', `media/resize/${size}/-/games/`);
-  return image;
+  if (imagePath) {
+    const image = imagePath.match(/media\/screenshots/)
+      ? imagePath.replace(
+          'media/screenshots',
+          `media/resize/${size}/-/screenshots`
+        )
+      : imagePath.replace('media/games/', `media/resize/${size}/-/games/`);
+    return image;
+  }
+  return imagePath;
 };
