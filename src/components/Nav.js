df44a7404a935736fee1fd8200d4b2815c7eaecf
Joseph M <jmedina1985@hotmail.com>
2021-01-24 23:28:25 -0800

    Adding Nav - 18

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -30,6 +30,12 @@ const StyledNav = styled(motion.nav)`
     margin-top: 1rem;
     box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.2);
   }
+  button {
+    font-size: 1.5rem;
+    border: none;
+    padding: 0.5rem 2rem;
+    cursor: pointer;
+  }
 `;
 const Logo = styled(motion.div)``;
 
