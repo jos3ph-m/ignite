44057153ec926f7a0b1d121da31c24b7963e9895
Joseph M <jmedina1985@hotmail.com>
2021-01-24 23:27:09 -0800

    Adding Nav - 17

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -45,6 +45,10 @@ const GlobalStyles = createGlobalStyle`
   img {
     display: block;
   }
+  input{
+    font-weight: bold;
+    font-family: 'Montserrat', sans-serif;
+  }
 `;
 
 export default GlobalStyles;
