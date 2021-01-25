d7b24cc50d2ff772e6f6619f175c42aa4342da7f
Joseph M <jmedina1985@hotmail.com>
2021-01-24 23:21:44 -0800

    Adding Nav - 11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 // Components and pages
 import Home from './pages/Home';
+import Nav from './components/Nav';
+// Styles
 import GlobalStyles from './components/GlobalStyles';
+// Router
 import { Route } from 'react-router-dom';
 
 function App() {
