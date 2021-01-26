de70b683edf9a5d4afdc9f198a5b75279f786f5c
Joseph M <jmedina1985@hotmail.com>
2021-01-25 19:40:07 -0800

    Nav searching - 1

--- src/api.js
+++ src/api.js
@@ -42,3 +42,6 @@ export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
 // Game screenshots
 export const gameScreenshotURL = (game_id) =>
   `${base_url}games/${game_id}/screenshots`;
+// Search Game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?search=${game_name}`;
